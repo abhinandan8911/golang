a668c59aa4ede0210169c1c0468d70c2fe7fac5d
Abhinandan Bhattacharya <abhinandan.bhattacharya@gmail.com>
2022-05-08 11:53:15 -0500

    changes

--- main/main.go
+++ main/main.go
@@ -4,10 +4,19 @@ import "fmt"
 
 type MyInt int
 
+type MyFunc func(message string)
+
+func Log(message string) {
+	fmt.Println("Hello ", message)
+}
+
 func main() {
 	var i interface{}
 	var mine MyInt
 	i = mine
 	i2 := i.(MyInt)
 	fmt.Println(i2 + 1)
+
+	myfunc := MyFunc(Log)
+	myfunc("Abhi")
 }
